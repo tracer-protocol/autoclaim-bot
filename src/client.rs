760f5068c5776221841623b0ffea0a232deb6d77
sporejack <jack.mcpherson@mycelium.ventures>
2022-03-14 13:29:24 +1000

    Allow `Client` to hand out middleware
    This commit adds a new function to the `Client` API allowing a `Client`
    type to create a new `SignerMiddleware` instance.

--- src/client.rs
+++ src/client.rs
@@ -1,13 +1,14 @@
+use ethers::prelude::SignerMiddleware;
 use ethers::providers::{JsonRpcClient, Provider};
 use ethers::signers::Signer;
 
 #[derive(Clone, Debug)]
-pub struct Client<S: Signer, P: JsonRpcClient> {
+pub struct Client<S: Signer + Clone, P: JsonRpcClient + Clone> {
     signer: Option<S>,
     provider: Provider<P>,
 }
 
-impl<S: Signer, P: JsonRpcClient> Client<S, P> {
+impl<S: Signer + Clone, P: JsonRpcClient + Clone> Client<S, P> {
     pub fn new(signer: Option<S>, provider: Provider<P>) -> Self {
         Self { signer, provider }
     }
@@ -18,5 +19,11 @@ impl<S: Signer, P: JsonRpcClient> Client<S, P> {
 
     pub fn provider(&self) -> &Provider<P> {
         &self.provider
+    }
+
+    pub fn middleware(&self) -> Option<SignerMiddleware<Provider<P>, S>> {
+        self.signer
+            .as_ref()
+            .map(|s| SignerMiddleware::new(self.provider.clone(), s.clone()))
     }
 }

22a99237c2d8ec9c55ec10b76a67b5d8c463fa0c
sporejack <jack.mcpherson@mycelium.ventures>
2022-03-16 11:41:06 +1000

    Define `CreateCommit` event
    This commit adds a type definition for the CreateCommit event.

--- src/pool.rs
+++ src/pool.rs
@@ -1,3 +1,27 @@
-use ethers::prelude::abigen;
+use ethers::prelude::{abigen, EthEvent};
+use ethers::types::{Address, U256};
 
 abigen!(LeveragedPool, "abis/LeveragedPool.json");
+
+pub const EVENT_NAME: &str = "CreateCommit";
+
+#[derive(Copy, Clone, Debug)]
+pub enum CommitType {
+    ShortMint,
+    ShortBurn,
+    LongMint,
+    LongBurn,
+    LongBurnShortMint,
+    ShortBurnLongMint,
+}
+
+#[derive(Clone, Debug, EthEvent)]
+pub struct CreateCommit {
+    user: Address,
+    amount: U256,
+    commit_type: U256,
+    appropriate_update_interval_id: U256,
+    from_aggregate_balance: bool,
+    pay_for_claim: bool,
+    minting_fee: [u8; 16],
+}
